96102f6a7e2cdb58c109282ddb4f35c117aed4cb
Chris Pulman <chris.pulman@yahoo.com>
2024-01-09 12:25:29 +0000

    Add Release Gate

--- build/Build.cs
+++ build/Build.cs
@@ -12,19 +12,19 @@ using Nuke.Common.Tools.PowerShell;
 using CP.BuildTools;
 using Nuke.Common.Tools.MSBuild;
 
-[GitHubActions(
-    "BuildOnly",
-    GitHubActionsImage.WindowsLatest,
-    OnPushBranchesIgnore = new[] { "main" },
-    FetchDepth = 0,
-    InvokedTargets = new[] { nameof(Compile) })]
-[GitHubActions(
-    "BuildDeploy",
-    GitHubActionsImage.WindowsLatest,
-    OnPushBranches = new[] { "main" },
-    FetchDepth = 0,
-    ImportSecrets = new[] { nameof(NuGetApiKey) },
-    InvokedTargets = new[] { nameof(Compile), nameof(Deploy) })]
+////[GitHubActions(
+////    "BuildOnly",
+////    GitHubActionsImage.WindowsLatest,
+////    OnPushBranchesIgnore = new[] { "main" },
+////    FetchDepth = 0,
+////    InvokedTargets = new[] { nameof(Compile) })]
+////[GitHubActions(
+////    "BuildDeploy",
+////    GitHubActionsImage.WindowsLatest,
+////    OnPushBranches = new[] { "main" },
+////    FetchDepth = 0,
+////    ImportSecrets = new[] { nameof(NuGetApiKey) },
+////    InvokedTargets = new[] { nameof(Compile), nameof(Deploy) })]
 partial class Build : NukeBuild
 {
     //// Support plugins are available for:
@@ -57,7 +57,7 @@ partial class Build : NukeBuild
             }
 
             PackagesDirectory.CreateOrCleanDirectory();
-            await this.InstallDotNetSdk("3.1.x", "5.x.x", "6.x.x", "7.x.x");
+            await this.InstallDotNetSdk("6.x.x", "7.x.x", "8.x.x");
         });
 
     Target Restore => _ => _
