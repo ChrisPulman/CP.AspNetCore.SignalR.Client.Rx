04636c42fe961b2e730109417a8bd354ec497190
Chris Pulman <chris.pulman@yahoo.com>
2025-01-16 20:59:37 +0000

    Update Code

--- src/WpfClient/App.xaml.cs
+++ src/WpfClient/App.xaml.cs
@@ -3,10 +3,9 @@
 
 using System.Windows;
 
-namespace SignalRChatClient
-{
-    /// <summary>
-    /// Interaction logic for App.xaml.
-    /// </summary>
-    public partial class App : Application;
-}
+namespace SignalRChatClient;
+
+/// <summary>
+/// Interaction logic for App.xaml.
+/// </summary>
+public partial class App : Application;
